98573f2bf6c359a4aa429a6298143d2d006b1c38
icaro paiva <icaro.paiva@acct.global>
2022-09-30 23:42:57 -0300

    fix provider context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,12 @@
 import React from "react"
-import { useSocketContext } from "../contexts/SocketContext"
-import { EnterRoom } from "../views/EnterRoom"
-import { Lobby } from "../views/Lobby"
-import { Tournment } from "../views/Tournment"
-import { Battle } from "../views/Battle"
+import { SocketProvider } from "../contexts/SocketContext"
+import { ViewsContainer } from "../views"
 
 const IndexPage = () => {
-  const { view } = useSocketContext()
-
   return (
-    <div className="main-container">
-      {view === "EnterRoom" && <EnterRoom />}
-      {view === "Lobby" && <Lobby />}
-      {view === "Tournment" && <Tournment />}
-      {view === "Battle" && <Battle />}
-    </div>
+    <SocketProvider>
+      <ViewsContainer />
+    </SocketProvider>
   )
 }
 
---
+++ src/views/index.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { useSocketContext } from "../contexts/SocketContext"
+import { EnterRoom } from "../views/EnterRoom"
+import { Lobby } from "../views/Lobby"
+import { Tournment } from "../views/Tournment"
+import { Battle } from "../views/Battle"
+
+export const ViewsContainer = () => {
+  const { view } = useSocketContext()
+
+  return (
+    <div className="main-container">
+      {view === "EnterRoom" && <EnterRoom />}
+      {view === "Lobby" && <Lobby />}
+      {view === "Tournment" && <Tournment />}
+      {view === "Battle" && <Battle />}
+    </div>
+  )
+}
