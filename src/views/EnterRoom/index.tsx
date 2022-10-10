be77b59c99af5a3c16f503ca4fe7b5bbb34eac4d
unknown <tallyta.martins@acct.global>
2022-10-10 20:33:30 -0300

    resolved the position of content

--- src/views/EnterRoom/index.tsx
+++ src/views/EnterRoom/index.tsx
@@ -1,9 +1,10 @@
-import { useRef, useState } from "react"
-import { useSocketContext } from "../../contexts/SocketContext"
-import { io } from "socket.io-client"
+import { useRef, useState } from 'react'
+import { useSocketContext } from '../../contexts/SocketContext'
+import { io } from 'socket.io-client'
+import './enterRoom.scss'
 
 export const EnterRoom = () => {
-  const [errorMsg, setErrorMsg] = useState("")
+  const [errorMsg, setErrorMsg] = useState('')
   const nameRef = useRef<HTMLInputElement | null>(null)
   const { setView, setSocket, idPlayer } = useSocketContext()
 
@@ -16,25 +17,25 @@ export const EnterRoom = () => {
     const name = nameRef.current?.value
 
     if (!name || name.length <= 2) {
-      setErrorMsg("Please, write a nickname with 3 characteres or more")
+      setErrorMsg('Please, write a nickname with 3 characteres or more')
       return
     }
 
     setSocket?.(
-      io(process.env.REACT_APP_API_URL ?? "http://localhost:3000").emit(
-        "player_connect",
+      io(process.env.REACT_APP_API_URL ?? 'http://localhost:3000').emit(
+        'player_connect',
         name
       )
     )
 
-    setView?.("Lobby")
+    setView?.('Lobby')
   }
 
   return (
-    <div>
+    <div className="container-enterRoom">
       <input type="text" ref={nameRef} />
-      <button onClick={handleConnect}>Connect</button>
-      {errorMsg && <p style={{ color: "red" }}>{errorMsg}</p>}
+      <button onClick={handleConnect}>Entrar</button>
+      {errorMsg && <p style={{ color: 'red' }}>{errorMsg}</p>}
     </div>
   )
 }
